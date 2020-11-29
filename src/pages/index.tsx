d5c02acf1546c654a51836351c934eb4a36bc2c4
Guilherme Faustino <guilherme.faustino_thera@prestador.globo>
2020-11-29 15:20:21 -0300

    add support huskys, prettier and eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+type Props = {
+  title: string
+}
+
+const name = 'gui'
+
+export default function Home                  ({ title = 'React Avançado!' }: Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
