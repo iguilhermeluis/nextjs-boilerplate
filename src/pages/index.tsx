0cef39e8f2d7ec0021d29e96326be92b52c0385b
Guilherme Faustino <guilherme.faustino_thera@prestador.globo>
2020-12-03 23:20:41 -0300

    add boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
-type Props = {
-  title: string
-}
-
-const name = 'gui'
-
-export default function Home({ title = 'React Avançado!' }: Props) {
-  return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
+export default function Home() {
+  return <Main />
 }
